0e6c6833f8972bf7ea78110f530d3a33aef10e4a
Joel <joelhernanmiranda@gmail.com>
2024-08-14 15:54:55 -0300

    mi primer repo con tailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                fucsiaCool: '#4a044e',
+            },
+        },
+    },
+    plugins: [],
+};
