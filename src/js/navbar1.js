3fdd06c8796b86d93108b57bbcd12504c7e083a9
Joel <joelhernanmiranda@gmail.com>
2024-11-20 15:36:52 -0300

    Agregando estilos a los navbars propios

--- src/js/navbar1.js
+++
@@ -1,9 +0,0 @@
-const icono = document.querySelector('#icono');
-const icon = document.querySelector('#icon');
-const close = `<i class="fa-solid fa-close text-4xl absolute left-[20px] cursor-pointer top-5 text-[#161D2D]" id="icono"></i>`;
-
-icono.addEventListener('change', (event) => {
-    if (icon.checked) {
-        icono.innerHTML = close;
-    }
-});
